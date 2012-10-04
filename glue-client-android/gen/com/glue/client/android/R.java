200fbf53f003bbce4cb3e3d03c99f4c39922af2d
Pascal Gillet <pascalgill3t@gmail.com>
2012-10-04 16:23:54 +0200

    Added Login activity and added French text for all the strings

--- glue-client-android/gen/com/glue/client/android/R.java
+++ glue-client-android/gen/com/glue/client/android/R.java
@@ -11,21 +11,41 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_launcher=0x7f020000;
+        public static final int ic_action_search=0x7f020000;
+        public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
-        public static final int item_detail=0x7f060002;
-        public static final int item_detail_container=0x7f060000;
-        public static final int item_list=0x7f060001;
+        public static final int button1=0x7f070005;
+        public static final int button2=0x7f070007;
+        public static final int editText2=0x7f070004;
+        public static final int editText3=0x7f070003;
+        public static final int imageView1=0x7f070002;
+        public static final int item_detail=0x7f070008;
+        public static final int item_detail_container=0x7f070000;
+        public static final int item_list=0x7f070001;
+        public static final int menu_settings=0x7f070009;
+        public static final int textView1=0x7f070006;
     }
     public static final class layout {
         public static final int activity_item_detail=0x7f030000;
         public static final int activity_item_list=0x7f030001;
         public static final int activity_item_twopane=0x7f030002;
-        public static final int fragment_item_detail=0x7f030003;
+        public static final int activity_login=0x7f030003;
+        public static final int fragment_item_detail=0x7f030004;
+    }
+    public static final class menu {
+        public static final int activity_login=0x7f060000;
     }
     public static final class string {
         public static final int app_name=0x7f040000;
+        public static final int hello_world=0x7f040003;
+        public static final int login=0x7f040008;
+        public static final int mail_address=0x7f040006;
+        public static final int menu_settings=0x7f040004;
+        public static final int or=0x7f04000a;
+        public static final int password=0x7f040007;
+        public static final int register=0x7f040009;
+        public static final int title_activity_login=0x7f040005;
         public static final int title_item_detail=0x7f040001;
         public static final int title_item_list=0x7f040002;
     }
