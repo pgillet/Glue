2ad2bc6e670ff1fbbf43ac4194f426625d282d99
Pascal Gillet <pascalgill3t@gmail.com>
2012-10-08 21:37:16 +0200

    Added a specific layout for landscape orientation for the Sign Up activity

--- glue-client-android/gen/com/glue/client/android/R.java
+++ glue-client-android/gen/com/glue/client/android/R.java
@@ -15,6 +15,7 @@ public final class R {
         public static final int ic_launcher=0x7f020001;
     }
     public static final class id {
+        public static final int TableLayout1=0x7f070012;
         public static final int button1=0x7f070005;
         public static final int button2=0x7f070007;
         public static final int editText1=0x7f07000a;
@@ -23,11 +24,12 @@ public final class R {
         public static final int editText4=0x7f07000e;
         public static final int editText5=0x7f070010;
         public static final int imageView1=0x7f070002;
-        public static final int item_detail=0x7f070011;
+        public static final int item_detail=0x7f070013;
         public static final int item_detail_container=0x7f070000;
         public static final int item_list=0x7f070001;
         public static final int layout01=0x7f070008;
-        public static final int menu_settings=0x7f070012;
+        public static final int layout02=0x7f070011;
+        public static final int menu_settings=0x7f070014;
         public static final int textView1=0x7f070006;
         public static final int textView2=0x7f070009;
         public static final int textView3=0x7f07000b;
