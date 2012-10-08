4a029f8e9340446cf0910effe45d3daecae9fb61
Pascal Gillet <pascalgill3t@gmail.com>
2012-10-08 22:24:40 +0200

    Filled the sign up form's email address with the first address found from the account manager

--- glue-client-android/src/com/glue/client/android/SignUpActivity.java
+++ glue-client-android/src/com/glue/client/android/SignUpActivity.java
@@ -1,20 +1,31 @@
 package com.glue.client.android;
 
-import android.os.Bundle;
+import android.accounts.Account;
+import android.accounts.AccountManager;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+import android.widget.TextView;
 
 public class SignUpActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_sign_up);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_sign_up);
+		Account[] accounts = AccountManager.get(this).getAccounts();
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_sign_up, menu);
-        return true;
-    }
+		if (accounts.length > 0) {
+			Account account = accounts[0];
+
+			final TextView tx = (TextView) findViewById(R.id.editText3);
+			tx.setText(account.name);
+		}
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_sign_up, menu);
+		return true;
+	}
 }
