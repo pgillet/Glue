f79050980646b13c984527c792dfa6f0281fdf1e
Pascal Gillet <pascalgill3t@gmail.com>
2012-10-11 23:59:14 +0200

    Added a drawable on the right to the text in the toggle button

--- glue-client-android/gen/com/glue/client/android/R.java
+++ glue-client-android/gen/com/glue/client/android/R.java
@@ -11,8 +11,10 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_action_search=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int device_access_not_secure=0x7f020000;
+        public static final int device_access_secure=0x7f020001;
+        public static final int ic_action_search=0x7f020002;
+        public static final int ic_launcher=0x7f020003;
     }
     public static final class id {
         public static final int button1=0x7f07000c;
--- glue-client-android/src/com/glue/client/android/CreateStreamMainActivity.java
+++ glue-client-android/src/com/glue/client/android/CreateStreamMainActivity.java
@@ -1,6 +1,8 @@
 package com.glue.client.android;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -26,11 +28,16 @@ public class CreateStreamMainActivity extends Activity {
 		ToggleButton toggle = (ToggleButton) view;
 		boolean on = toggle.isChecked();
 		TextView tv = (TextView) findViewById(R.id.textView4);
-
+		Resources res = getResources();
+		Drawable drawable = null;
 		if (on) {
+			drawable = res.getDrawable(R.drawable.device_access_not_secure);
 			tv.setText(R.string.public_description);
 		} else {
+			drawable = res.getDrawable(R.drawable.device_access_secure);
 			tv.setText(R.string.private_description);
 		}
+		toggle.setCompoundDrawablesWithIntrinsicBounds(null, null, drawable,
+				null);
 	}
 }
