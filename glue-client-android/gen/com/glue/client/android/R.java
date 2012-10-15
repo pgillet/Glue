542a1df88b632adc361bb07bcfb57ab7a1a9c54b
Pascal Gillet <pascalgill3t@gmail.com>
2012-10-15 11:06:13 +0200

    Factorized separator style

--- glue-client-android/gen/com/glue/client/android/R.java
+++ glue-client-android/gen/com/glue/client/android/R.java
@@ -21,33 +21,35 @@ public final class R {
         public static final int social_group=0x7f020007;
     }
     public static final class id {
-        public static final int TableLayout1=0x7f07000d;
+        public static final int LinearLayout1=0x7f07000d;
         public static final int button1=0x7f07000c;
         public static final int button2=0x7f07000b;
         public static final int editText1=0x7f070004;
         public static final int editText2=0x7f070006;
-        public static final int editText3=0x7f070014;
-        public static final int editText4=0x7f070017;
-        public static final int editText5=0x7f070019;
-        public static final int footer=0x7f070012;
-        public static final int header=0x7f070010;
-        public static final int imageView1=0x7f070011;
-        public static final int item_detail=0x7f07001a;
-        public static final int item_detail_container=0x7f07000e;
-        public static final int item_list=0x7f07000f;
-        public static final int layout01=0x7f070015;
+        public static final int editText3=0x7f070016;
+        public static final int editText4=0x7f070019;
+        public static final int editText5=0x7f07001b;
+        public static final int footer=0x7f070014;
+        public static final int header=0x7f070012;
+        public static final int imageView1=0x7f070013;
+        public static final int item_detail=0x7f07001c;
+        public static final int item_detail_container=0x7f070010;
+        public static final int item_list=0x7f070011;
+        public static final int layout01=0x7f070017;
+        public static final int layout02=0x7f07000e;
         public static final int layout03=0x7f070002;
         public static final int layout04=0x7f070000;
         public static final int layout05=0x7f070001;
         public static final int layout06=0x7f07000a;
-        public static final int loginForm=0x7f070013;
-        public static final int menu_settings=0x7f07001b;
+        public static final int layout07=0x7f07000f;
+        public static final int loginForm=0x7f070015;
+        public static final int menu_settings=0x7f07001d;
         public static final int textView1=0x7f070003;
         public static final int textView2=0x7f070005;
         public static final int textView3=0x7f070007;
         public static final int textView4=0x7f070008;
-        public static final int textView5=0x7f070016;
-        public static final int textView6=0x7f070018;
+        public static final int textView5=0x7f070018;
+        public static final int textView6=0x7f07001a;
         public static final int toggleButton1=0x7f070009;
     }
     public static final class layout {
@@ -59,6 +61,7 @@ public final class R {
         public static final int activity_login=0x7f030005;
         public static final int activity_sign_up=0x7f030006;
         public static final int fragment_item_detail=0x7f030007;
+        public static final int separator=0x7f030008;
     }
     public static final class menu {
         public static final int activity_create_stream_main=0x7f060000;
@@ -83,8 +86,10 @@ public final class R {
         public static final int open=0x7f04001e;
         public static final int open_description=0x7f040021;
         public static final int or=0x7f04000a;
+        public static final int participants=0x7f040023;
         public static final int participation_type=0x7f040022;
         public static final int password=0x7f040007;
+        public static final int press_enter_to_add_a_participant=0x7f040024;
         public static final int press_to_enter_a_title=0x7f04001b;
         public static final int press_to_enter_one_or_more_tags=0x7f04001c;
         public static final int privacy=0x7f040018;
