67c3b2177c73b6e0ab3bbeca6a5528de3b1b62f4
Pascal Gillet <pascalgill3t@gmail.com>
2012-10-18 01:16:28 +0200

    Added an auto-complete text view for the contact list

--- glue-client-android/src/com/glue/client/android/CreateStreamUserActivity.java
+++ glue-client-android/src/com/glue/client/android/CreateStreamUserActivity.java
@@ -5,12 +5,17 @@ import android.content.Intent;
 import android.content.res.Resources;
 import android.database.Cursor;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
 import android.provider.ContactsContract.Contacts;
 import android.view.Menu;
 import android.view.View;
+import android.widget.AutoCompleteTextView;
 import android.widget.Button;
-import android.widget.EditText;
+import android.widget.FilterQueryProvider;
+import android.widget.SimpleCursorAdapter;
+import android.widget.SimpleCursorAdapter.CursorToStringConverter;
 import android.widget.TextView;
 import android.widget.ToggleButton;
 
@@ -19,18 +24,85 @@ import com.glue.client.android.view.FlowLayout;
 public class CreateStreamUserActivity extends Activity {
 
 	static final int PICK_CONTACT_REQUEST = 0;
-	private EditText editText;
 	private FlowLayout contactList;
 	private TextView tv;
+
+	private boolean mShowInvisible;
+	private AutoCompleteTextView textView;
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_create_stream_user);
-		
+
 		tv = (TextView) findViewById(R.id.textView1);
-		editText = (EditText) findViewById(R.id.editText1);
 		contactList = (FlowLayout) findViewById(R.id.contactList);
+
+		textView = (AutoCompleteTextView) findViewById(R.id.autocomplete_contacts);
+		populateContactList();
+	}
+
+	/**
+	 * Obtains the contact list for the currently selected account.
+	 * 
+	 * @return A cursor for for accessing the contact list.
+	 */
+	private Cursor getContacts(String arg) {
+		// Run query
+		Uri uri = ContactsContract.Contacts.CONTENT_URI;
+		String[] projection = new String[] { ContactsContract.Contacts._ID,
+				ContactsContract.Contacts.DISPLAY_NAME };
+		// String selection = ContactsContract.Contacts.IN_VISIBLE_GROUP +
+		// " = '"
+		// + (mShowInvisible ? "0" : "1") + "'";
+
+		String selection = null;
+		if (arg != null) {
+			selection = ContactsContract.Contacts.DISPLAY_NAME + " LIKE '%"
+					+ arg + "%'";
+		}
+
+		String[] selectionArgs = null;
+		String sortOrder = ContactsContract.Contacts.DISPLAY_NAME
+				+ " COLLATE LOCALIZED ASC";
+
+		return managedQuery(uri, projection, selection, selectionArgs,
+				sortOrder);
+	}
+
+	/**
+	 * Populate the contact list based on account currently selected in the
+	 * account spinner.
+	 */
+	private void populateContactList() {
+		// Build adapter with contact entries
+		Cursor cursor = getContacts(null);
+		String[] fields = new String[] { ContactsContract.Data.DISPLAY_NAME };
+		SimpleCursorAdapter adapter = new SimpleCursorAdapter(this,
+				R.layout.contact_entry, cursor, fields,
+				new int[] { R.id.contactEntryText });
+
+		adapter.setCursorToStringConverter(new CursorToStringConverter() {
+
+			@Override
+			public String convertToString(android.database.Cursor cursor) {
+				// Get the label for this row out of the "state" column
+				final int columnIndex = cursor
+						.getColumnIndexOrThrow(ContactsContract.Data.DISPLAY_NAME);
+				final String str = cursor.getString(columnIndex);
+				return str;
+			}
+		});
+
+		adapter.setFilterQueryProvider(new FilterQueryProvider() {
+
+			@Override
+			public Cursor runQuery(CharSequence constraint) {
+				return getContacts(constraint.toString());
+			}
+		});
+
+		textView.setAdapter(adapter);
 	}
 
 	@Override
@@ -42,7 +114,7 @@ public class CreateStreamUserActivity extends Activity {
 	public void onClickToggle(View view) {
 		ToggleButton toggle = (ToggleButton) view;
 		boolean on = toggle.isChecked();
-		
+
 		Resources res = getResources();
 		Drawable drawable = null;
 		if (on) {
