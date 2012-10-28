06533c6990921960b4664fbb15d832f32f58f1b3
Pascal Gillet <pascalgill3t@gmail.com>
2012-10-28 11:57:46 +0100

    Added controls for the user to pick a time or pick a date as dialogs

--- glue-client-android/src/com/glue/client/android/CreateStreamLocationActivity.java
+++ glue-client-android/src/com/glue/client/android/CreateStreamLocationActivity.java
@@ -1,20 +1,39 @@
 package com.glue.client.android;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v4.app.DialogFragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
+import android.view.View;
 
-public class CreateStreamLocationActivity extends Activity {
+import com.glue.client.android.dialog.DatePickerFragment;
+import com.glue.client.android.dialog.TimePickerFragment;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_create_stream_location);
-    }
+public class CreateStreamLocationActivity extends FragmentActivity {
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_create_stream_location, menu);
-        return true;
-    }
+	private static final String TIME_PICKER = "timePicker";
+	private static final String DATE_PICKER = "datePicker";
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_create_stream_location);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		getMenuInflater().inflate(R.menu.activity_create_stream_location, menu);
+		return true;
+	}
+
+	public void onClickFromDate(View v) {
+		DialogFragment newFragment = new DatePickerFragment();
+		newFragment.show(getSupportFragmentManager(), DATE_PICKER);
+	}
+
+	public void onClickFromTime(View v) {
+		DialogFragment newFragment = new TimePickerFragment();
+		newFragment.show(getSupportFragmentManager(), TIME_PICKER);
+	}
+
 }
