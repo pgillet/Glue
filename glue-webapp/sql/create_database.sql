2bfe0835823498cb6853cf4a3fea21b0a3315db5
grdenis <gregoire.denis.31@gmail.com>
2012-11-29 21:17:49 +0100

    SQL schema completed + createStream update

--- glue-webapp/sql/create_database.sql
+++ glue-webapp/sql/create_database.sql
@@ -11,23 +11,82 @@ USE gluedb;
 -- Tables
 
 -- User table
--- CREATE TABLE user (
--- );
+CREATE TABLE GLUE_USER (
+			id BIGINT NOT NULL AUTO_INCREMENT,
+			first_name VARCHAR(50) NOT NULL,
+			last_name VARCHAR(50) NOT NULL,
+            email VARCHAR(50) NOT NULL,
+            passwd VARCHAR(50) NOT NULL,
+            PRIMARY KEY (id),
+            UNIQUE (email)
+			);
 
 -- Stream table
-CREATE TABLE stream (
+CREATE TABLE STREAM (
              id BIGINT NOT NULL AUTO_INCREMENT,
-             title VARCHAR(30) NOT NULL,
-             description VARCHAR(255),
+             title VARCHAR(50) NOT NULL,
              public BOOL NOT NULL,
              open BOOL NOT NULL,
-             secretQuestion VARCHAR(100) DEFAULT NULL,
-             secretAnswer VARCHAR(100) DEFAULT NULL,
-             requestToParticipate BOOL NOT NULL,
-             startDate BIGINT UNSIGNED DEFAULT NULL,
-             endDate BIGINT UNSIGNED DEFAULT NULL,
-             latitude DECIMAL(10,8) DEFAULT NULL,
-             longitude DECIMAL(10,8) DEFAULT NULL,
-             address VARCHAR(255) DEFAULT NULL,
+             secret_question VARCHAR(100),
+             secret_Answer VARCHAR(100),
+             request_to_participate BOOL NOT NULL,
+             start_date BIGINT UNSIGNED NOT NULL,
+             end_date BIGINT UNSIGNED,
+             latitude DECIMAL(10,8),
+             longitude DECIMAL(10,8),
+             address VARCHAR(255),
              PRIMARY KEY (id)
              );
+             
+-- TAG table
+CREATE TABLE TAG (
+             id BIGINT NOT NULL AUTO_INCREMENT,
+             tag VARCHAR(50) NOT NULL,
+             stream_id BIGINT NOT NULL,
+             PRIMARY KEY (id),
+             INDEX (tag),
+             FOREIGN KEY (stream_id) REFERENCES STREAM(id)
+                     ON DELETE CASCADE
+             );
+             
+-- Invited table
+CREATE TABLE INVITED (
+			id BIGINT NOT NULL AUTO_INCREMENT,
+			name VARCHAR(50) DEFAULT NULL,
+			email VARCHAR(50) NOT NULL,
+            stream_id BIGINT NOT NULL,
+            guest_only BOOL NOT NULL,
+            PRIMARY KEY (id),
+            FOREIGN KEY (stream_id) REFERENCES STREAM(id)
+                     ON DELETE CASCADE
+			);
+			
+-- PARTICIPANT table
+CREATE TABLE PARTICIPANT (
+			id BIGINT NOT NULL AUTO_INCREMENT,
+			user_id BIGINT NOT NULL,
+			stream_id BIGINT NOT NULL,
+			admin BOOL NOT NULL,
+			PRIMARY KEY (id),
+			FOREIGN KEY (user_id) REFERENCES GLUE_USER(id),
+            FOREIGN KEY (stream_id) REFERENCES STREAM(id)
+                     ON DELETE CASCADE
+			);
+			
+			
+-- MEDIA table
+CREATE TABLE MEDIA (
+			id BIGINT NOT NULL AUTO_INCREMENT,
+			user_id BIGINT NOT NULL,
+			stream_id BIGINT NOT NULL,
+			extension VARCHAR(5) NOT NULL,
+			mime_type ENUM('Audio','Picture','Text') NOT NULL,
+			caption VARCHAR(50),
+			latitude DECIMAL(10,8),
+            longitude DECIMAL(10,8),
+            start_date BIGINT UNSIGNED NOT NULL,
+			PRIMARY KEY (id),
+			FOREIGN KEY (user_id) REFERENCES GLUE_USER(id),
+			FOREIGN KEY (stream_id) REFERENCES STREAM(id)
+                     ON DELETE CASCADE
+			);
