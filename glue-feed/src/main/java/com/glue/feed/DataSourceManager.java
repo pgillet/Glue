3417e581b326ab41b4b1e381aabd83ff726a4823
grdenis <gregoire.denis.31@gmail.com>
2014-01-07 11:46:34 +0100

    Merge conflict.

--- glue-feed/src/main/java/com/glue/feed/DataSourceManager.java
+++ glue-feed/src/main/java/com/glue/feed/DataSourceManager.java
@@ -6,6 +6,18 @@ import com.mysql.jdbc.jdbc2.optional.MysqlDataSource;
 
 public class DataSourceManager {
 
+	private static final String DEFAULT_SERVER = "localhost";
+	private static final int DEFAULT_PORT_NUMBER = 3306;
+	private static final String DEFAULT_DATABASE_NAME = "gluedb";
+	private static final String DEFAULT_USER = "glue";
+	private static final String DEFAULT_PASSWORD = "glue";
+	
+	private String serverName = DEFAULT_SERVER;
+	private int portNumber = DEFAULT_PORT_NUMBER;
+	private String databaseName = DEFAULT_DATABASE_NAME;
+	private String user = DEFAULT_USER;
+	private String password = DEFAULT_PASSWORD;
+	
 	private static DataSourceManager factory = new DataSourceManager();
 	private MysqlDataSource ds;
 
@@ -20,14 +32,54 @@ public class DataSourceManager {
 
 		if (ds == null) {
 			ds = new MysqlDataSource();
-			ds.setServerName("localhost");
-			ds.setPortNumber(3306);
-			ds.setDatabaseName("gluedb");
-			ds.setUser("glue");
-			ds.setPassword("glue");
+			ds.setServerName(serverName);
+			ds.setPortNumber(portNumber);
+			ds.setDatabaseName(databaseName);
+			ds.setUser(user);
+			ds.setPassword(password);
 		}
 
 		return ds;
+	}
+
+	public String getServerName() {
+		return serverName;
+	}
+
+	public void setServerName(String serverName) {
+		this.serverName = serverName;
+	}
+
+	public int getPortNumber() {
+		return portNumber;
+	}
+
+	public void setPortNumber(int portNumber) {
+		this.portNumber = portNumber;
+	}
+
+	public String getDatabaseName() {
+		return databaseName;
+	}
+
+	public void setDatabaseName(String databaseName) {
+		this.databaseName = databaseName;
+	}
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
 	}
 
 }
