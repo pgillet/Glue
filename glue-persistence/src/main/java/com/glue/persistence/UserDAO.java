281ae2b3f8b6393af5a127dffc87b569f6b53c8b
Pascal Gillet <pascalgill3t@gmail.com>
2014-03-12 11:45:44 +0100

    Replaced the named queries by query strings

--- glue-persistence/src/main/java/com/glue/persistence/UserDAO.java
+++ glue-persistence/src/main/java/com/glue/persistence/UserDAO.java
@@ -1,14 +1,9 @@
 package com.glue.persistence;
 
-import javax.persistence.NamedQueries;
-import javax.persistence.NamedQuery;
 import javax.persistence.Query;
 
 import com.glue.domain.User;
 
-@NamedQueries({
-	@NamedQuery(name = "findByEmail", query = "SELECT u FROM User u WHERE u.email = :email"),
-	@NamedQuery(name = "findByCredentials", query = "SELECT u FROM User u WHERE u.email = :email AND u.password = :password") })
 public class UserDAO extends AbstractDAO<User> {
 
     public UserDAO() {
@@ -16,7 +11,10 @@ public class UserDAO extends AbstractDAO<User> {
     }
 
     public User authenticate(String email, String password) {
-	Query query = em.createNamedQuery("findByCredentials", type)
+
+	final String queryString = "SELECT u FROM User u WHERE u.email = :email AND u.password = :password";
+
+	Query query = em.createQuery(queryString, type)
 		.setParameter("email", email)
 		.setParameter("password", password);
 
@@ -26,8 +24,11 @@ public class UserDAO extends AbstractDAO<User> {
     }
 
     public User findByEmail(String email) {
-	Query query = em.createNamedQuery("findByEmail", type).setParameter(
-		"email", email);
+
+	final String queryString = "SELECT u FROM User u WHERE u.email = :email";
+
+	Query query = em.createQuery(queryString, type).setParameter("email",
+		email);
 
 	User user = PersistenceHelper.getSingleResultOrNull(query);
 
