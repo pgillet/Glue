d094b0843ee49ebd3a9979e008d9ae9400f5704d
Pascal Gillet <pascalgill3t@gmail.com>
2014-05-21 17:04:13 +0200

    Avoids calling Jsoup#clean when the given html body is null, empty or whitespace

--- glue-webapp/src/main/java/com/glue/webapp/logic/EventUtils.java
+++ glue-webapp/src/main/java/com/glue/webapp/logic/EventUtils.java
@@ -11,9 +11,13 @@ public class EventUtils {
     private static final int SUMMARY_LIMIT = 200;
 
     public static String summarize(String html) {
+
+	if (StringUtils.isBlank(html)) {
+	    return null;
+	}
+
 	// Keep only text nodes: all HTML will be stripped.
-	String onlytext = Jsoup.clean(StringUtils.defaultString(html),
-		Whitelist.none());
+	String onlytext = Jsoup.clean(html, Whitelist.none());
 	String summary = WordUtils.abbreviate(onlytext, SUMMARY_LIMIT, -1,
 		ELLIPSIS);
 
