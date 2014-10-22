dc440477fa64d6c404cc593850752d5c492f6cd1
grdenis <gregoire.denis.31@gmail.com>
2014-10-22 10:52:47 +0200

    Fix duplicate Tag Bug.

--- glue-domain/src/main/java/com/glue/domain/Tag.java
+++ glue-domain/src/main/java/com/glue/domain/Tag.java
@@ -111,7 +111,7 @@ public class Tag {
 	if (title == null) {
 	    if (other.title != null)
 		return false;
-	} else if (!title.equals(other.title))
+	} else if (!title.equalsIgnoreCase(other.title))
 	    return false;
 	return true;
     }
